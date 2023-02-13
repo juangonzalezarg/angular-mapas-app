827e1c88a72a5109df603360238385d43803fe66
Juan Manuel González <juangonzalez664@gmail.com>
2023-02-13 14:50:21 -0300

    first commit

---
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-full-screen',
+  templateUrl: './full-screen.component.html',
+  styles: [
+    `
+    #mapa {
+      width: 100%;
+      height: 100%;
+    }
+    `
+  ]
+})
+export class FullScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-54.70298858290487, -26.469803687019066],
+      zoom: 18
+    });
+  }
+
+}
